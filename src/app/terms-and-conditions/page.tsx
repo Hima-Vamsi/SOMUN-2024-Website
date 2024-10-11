5e2e0cc6bcb11c3a2a8ea5be112eedf969eaa623
Katta Hima Vamsi <hvkatta@gmail.com>
2024-10-11 15:14:59 +0530

    Uninstall unrequired packages, update Terms and Conditions page

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -1,9 +1,21 @@
-import PdfViewer from "@/components/pdf-viewer";
-const termsAndConditions = () => {
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const TermsAndConditions = () => {
   return (
-    <>
-      <PdfViewer url={"/SOMUN 2024 Terms and Conditions.pdf"} />
-    </>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">
+        SOMUN 2024 Terms and Conditions
+      </h1>
+      <Link
+        href="/SOMUN 2024 Terms and Conditions.pdf"
+        target="_blank"
+        download
+      >
+        <Button>Download Terms and Conditions</Button>
+      </Link>
+    </div>
   );
 };
-export default termsAndConditions;
+
+export default TermsAndConditions;
